5d38e4184e62bb66c7d5640ac35789661ef3815e
Luciana Miguel <luciana_miguel2004@hotmail.com>
2024-01-23 23:52:40 -0300

    commit inicial

---
+++ aula2.py
@@ -0,0 +1,17 @@
+nome= "João"
+
+ano_de_nascimento = 2000
+idade = 2023 - ano_de_nascimento
+maior_de_idade= idade >= 18
+sobrenome= "Silva"
+altura= 1.63
+
+
+
+
+print("Nome", nome)
+print("sobrenome", sobrenome)
+print("idade", idade)
+print("ano de nascimento", ano_de_nascimento)
+print("é maior de idade?", maior_de_idade)
+print("altura em metros:", nome)
---
+++ aula21.py
@@ -0,0 +1,5 @@
+nome = "João"
+peso = 57.5
+altura = 1.74
+imc = peso/(altura) ** 2
+print (nome , "tem" , peso , "kg e tem ", altura , "seu IMC é", imc )
